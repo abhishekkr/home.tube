e8c30fc9db612f53b0b8a00e61f5a7b0f5976c31
AbhishekKr <abhikumar163@gmail.com>
2024-08-20 20:29:32 +0530

    fix media seek; add audio.json reliant audio element source
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,18 +1,65 @@
 const mediaThumbnail = document.getElementById('media-thumbnail');
 const mediaPlayer = document.getElementById('media-player');
+const mediaPlayerSrc = document.getElementById('media-player-src');
+const mediaTitle = document.getElementById('media-title');
+const mediaDesc = document.getElementById('media-description');
 
 const mediaSeeker = document.getElementById('media-seeker');
 const btnPlay = document.getElementById('btn-play');
 const btnPause = document.getElementById('btn-pause');
 
-let syncTheSeeker = false;
+class MediaContent {
+  constructor(name, url) {
+    let split_name = name.split(" - ");
+    this.title = split_name[0];
+    this.description = split_name[1];
+    this.url = url;
+  }
+
+  play(){
+    mediaPlayerSrc.src = this.url;
+    mediaPlayer.load();
+    mediaTitle.innerHTML = this.title;
+    mediaDesc.innerHTML = this.description;
+  }
+}
+
+var audioContent = {};
+var contentToPlay;
+const request = new XMLHttpRequest();
+try {
+  request.open("GET", "media/audio.json");
+  request.responseType = "json";
+  request.addEventListener("load", () => {
+      // audioContent = JSON.parse(request.response);
+      audioContent = request.response;
+      for (ado in audioContent) {
+        console.log(ado);
+        contentToPlay = new MediaContent(ado, audioContent[ado]);
+        break;
+        // all will be managed later; when Selection/Playlist is in play
+      }
+      console.log(contentToPlay);
+      contentToPlay.play();
+  });
+  request.addEventListener("error", () => console.error("XHR error"));
+  request.send();
+} catch (error) {
+  console.error(`XHR error ${request.status}`);
+}
+
+
+var syncTheSeeker = false;
+var syncTheSeekerTimer;
 
 mediaPlayer.addEventListener('play', () => {
   mediaThumbnail.src = 'img/music.gif';
   btnPause.style.display = 'block';
   btnPlay.style.display = 'none';
-  mediaSeeker.max = mediaPlayer.duration;
-  var syncTheSeekerTimer = setInterval(() => {
+  if (syncTheSeekerTimer) {
+    clearInterval(syncTheSeekerTimer);
+  }
+  syncTheSeekerTimer = setInterval(() => {
       mediaSeeker.value = mediaPlayer.currentTime;
       if (syncTheSeeker) {
         clearInterval(syncTheSeekerTimer);
@@ -33,7 +80,6 @@ mediaPlayer.addEventListener('ended', () => {
 });
 
 mediaPlayer.addEventListener('loadedmetadata', () => {
-  console.log('Media metadata loaded.');
   console.log(`The duration of the video is ${mediaPlayer.duration} seconds.`);
   mediaSeeker.max = mediaPlayer.duration;
   mediaSeeker.value = mediaPlayer.currentTime;
@@ -65,12 +111,4 @@ function logEvent(event) {
   console.log(`Media Event: ${event.type}\n`);
   console.log(event);
 }
-
-mediaPlayer.addEventListener("loadstart", logEvent);
-mediaPlayer.addEventListener("loadedmetadata", logEvent);
-mediaPlayer.addEventListener("progress", logEvent);
-mediaPlayer.addEventListener("canplay", logEvent);
-mediaPlayer.addEventListener("canplaythrough", logEvent);
-mediaPlayer.addEventListener("seeked", logEvent);
-mediaPlayer.addEventListener("seeking", logEvent);
 mediaPlayer.addEventListener("error", logEvent);
